3c3d18f49410d7eb47948751b5dded6c799a7f80
Gavin <waltersg298@gmail.com>
2023-06-08 19:08:28 -0600

    building doors

---
+++ testmodules/one_hundred_doors.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+class OneHundredDoors:
+    def __init__(self):
+        self.doors = [] * 100
+
+
+if __name__ == '__main__':
+
+    one_hundred_doors = OneHundredDoors()
+
